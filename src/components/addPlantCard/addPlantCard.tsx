399b1110869ec2768b51e1a909c2c53694f25a8a
shubhamtelansol <shubhamt@elansoltech.com>
2025-05-27 13:08:14 +0530

    remove the inline css from addPlant, plantInfo and imageUpload component

--- src/components/addPlantCard/addPlantCard.tsx
+++ src/components/addPlantCard/addPlantCard.tsx
@@ -1,24 +1,17 @@
 import { Card, CardContent, Typography, Box } from '@mui/material';
 import React from 'react';
 import { AddPlantCardProps } from './AddPlantCard.d';
+import AddPlantLogo from '../../../public/images/plant-logo.png';
+import styles from './style.module.css';
 function AddPlantCard({ label, backgroundColor, onClick }: AddPlantCardProps) {
   return (
     <Box>
       <Card
-        sx={{
-          maxWidth: 345,
-          cursor: 'pointer',
-          '&:hover': { boxShadow: 3 },
-          background: backgroundColor,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          height: '280px',
-          boxShadow: '1px solid #837878',
-        }}
+        className={styles.addPlantCard}
+        style={{ background: backgroundColor }} // Keep dynamic background inline
         onClick={onClick}
       >
+        {/* this is an svg icon for plant  */}
         <svg width="48" height="48" viewBox="0 0 48 48" fill="none">
           <path
             d="M4 42V6H23.5V14.25H44V31.25H41V17.25H23.5V22.5H27.5V25.5H23.5V30.75H27.5V33.75H23.5V39H33.25V42H4ZM7 39H12.25V33.75H7V39ZM7 30.75H12.25V25.5H7V30.75ZM7 22.5H12.25V17.25H7V22.5ZM7 14.25H12.25V9H7V14.25ZM15.25 39H20.5V33.75H15.25V39ZM15.25 30.75H20.5V25.5H15.25V30.75ZM15.25 22.5H20.5V17.25H15.25V22.5ZM15.25 14.25H20.5V9H15.25V14.25ZM40.65 46V41.65H36.25V38.65H40.65V34.25H43.65V38.65H48V41.65H43.65V46H40.65ZM32.75 25.5V22.5H35.75V25.5H32.75ZM32.75 33.75V30.75H35.75V33.75H32.75Z"
