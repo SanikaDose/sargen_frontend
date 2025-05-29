d1b926105d7b4444f3292edf314faf971bb485ad
Virenpatil-elansol <virenp@elansoltech.com>
2025-05-29 17:53:23 +0530

    Setup with rtk

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import GlobalReduxProvider from '@/providers/GlobalReduxProvider';
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
@@ -26,7 +27,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        <ThemeProvider>{children}</ThemeProvider>
+        <ThemeProvider>
+          <GlobalReduxProvider>{children}</GlobalReduxProvider>
+        </ThemeProvider>
       </body>
     </html>
   );
