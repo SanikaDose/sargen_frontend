44cc8b5969ea2bd22cc713d3f3f10fe9f11a85f0
harshalelansol <harshald@elansoltech.com>
2025-06-04 18:03:22 +0530

    back button functionality

--- src/app/(protectedRoutes)/(organisation)/(contactPerson)/ContactPersonForm.tsx
+++ src/app/(protectedRoutes)/(organisation)/(contactPerson)/ContactPersonForm.tsx
@@ -283,7 +283,7 @@ const ContactPersonForm = ({ tenantId, editMode = false }: ContactPersonFormProp
           variant="contained"
           icon="left"
           color="#10557C"
-          onClick={() => router.push('/OrganizationOnboarding')}
+          onClick={() => router.push('/OrganisationOnboarding')}
         >
           Back
         </CustomButton>
