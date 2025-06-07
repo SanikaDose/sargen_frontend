023240623c2dfe69825bd26f826faf47a93c18c2
Virenpatil-elansol <virenp@elansoltech.com>
2025-06-08 02:37:27 +0530

    Added the navbar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,5 @@
 'use client';
 
-import Loader from '@/components/Loader/Loader';
-import { useForm } from 'react-hook-form';
-
 export default function Home() {
-  return <h1>dslfkji</h1>
+  return <h1>This is the home page without any route</h1>;
 }
