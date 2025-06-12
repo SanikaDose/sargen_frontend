b0aa8f01e6c8cf2b18a88af01bfbdcb94fc43242
tarjan-elansol <tarjanb@elansoltech.com>
2025-06-12 11:41:17 +0530

    added theme pallets to the custom button

--- src/app/(protectedRoutes)/(organisation)/(plant)/AddPlant/PlantRegistrationForm.tsx
+++ src/app/(protectedRoutes)/(organisation)/(plant)/AddPlant/PlantRegistrationForm.tsx
@@ -175,10 +175,10 @@ const PlantRegistrationForm = () => {
       </Box>
 
       <Box className={styles.buttonSection}>
-        <CustomButton children="Back" variant="contained" color="primary" icon="left" type="button" />
+        <CustomButton children="Back" variant="outlined" color="primary" icon="left" type="button" />
         <CustomButton
           children={isLoading ? 'Saving...' : 'Save'}
-          variant="contained"
+          variant="outlined"
           color="primary"
           icon="save"
           type="submit"
