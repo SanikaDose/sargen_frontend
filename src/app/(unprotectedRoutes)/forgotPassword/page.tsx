09a9447852dc2e01ba5eacfe6701545f0e89e9a2
tarjan-elansol <tarjanb@elansoltech.com>
2025-06-22 12:54:52 +0530

    fixed input of forget password

--- src/app/(unprotectedRoutes)/forgotPassword/page.tsx
+++ src/app/(unprotectedRoutes)/forgotPassword/page.tsx
@@ -7,6 +7,7 @@ import { useRouter } from 'next/navigation';
 import styles from './style.module.css';
 import { useForgotPasswordApiMutation } from '../login/loginApi';
 import { pageRoutes } from '@/constants/pagesRoutes';
+import { Height } from '@mui/icons-material';
 
 type FormValues = {
   email: string;
@@ -14,6 +15,13 @@ type FormValues = {
 
 const inputSx = {
   backgroundColor: '#fff',
+  borderRadius: '16px',
+  fontSize: '0.9rem',
+  padding: '10px 14px',
+  height: '35px',
+  '& .MuiOutlinedInput-notchedOutline': {
+    borderRadius: '16px',
+  },
 };
 
 const ForgotPasswordPage = ({ setForgotPassword }: { setForgotPassword: (val: boolean) => void }) => {
