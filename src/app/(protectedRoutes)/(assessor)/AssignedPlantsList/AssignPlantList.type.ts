812829c77eeb41ecd51e8a0b1d13a0327092b9dc
shubhamtelansol <shubhamt@elansoltech.com>
2025-06-24 19:06:49 +0530

    COMMIT BEFORE PULL

---
+++ src/app/(protectedRoutes)/(assessor)/AssignedPlantsList/AssignPlantList.type.ts
@@ -0,0 +1,29 @@
+export type Plant = {
+  assessmentDate: string;
+  id: string;
+  name: string;
+  location: string;
+  registrationNo: string;
+  revenue: string;
+  currencyType: string;
+  type: string;
+  age: number;
+  numberOfEmployees: number;
+  numberOfLines: number;
+  gstin: string;
+  createdAt: string;
+  updatedAt: string;
+  createdBy: string | null;
+  updatedBy: string | null;
+  assessmentStartDate: string;
+  debriefDate: string;
+  about: string;
+  assessor: string;
+  assessmentTableAssignedList: string[];
+  assessmentCompletionPercentage: number;
+  assessmentCompletionStage: 'NOT_STARTED' | 'STARTED' | 'START_ASSESSMENT' | 'REQUESTED_ASSESSMENT' | 'COMPLETED' | string; // include `string` fallback if values may vary
+  plantLogo: string | null;
+  pocFullName: string;
+  pocEmail: string;
+  pocContactNo: string;
+};
