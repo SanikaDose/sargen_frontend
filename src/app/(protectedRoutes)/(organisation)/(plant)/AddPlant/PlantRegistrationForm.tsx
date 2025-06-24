2b594d696bf53927f073837250a88aec8f1d1581
harshalelansol <harshald@elansoltech.com>
2025-06-24 16:51:59 +0530

    bug fixes

--- src/app/(protectedRoutes)/(organisation)/(plant)/AddPlant/PlantRegistrationForm.tsx
+++ src/app/(protectedRoutes)/(organisation)/(plant)/AddPlant/PlantRegistrationForm.tsx
@@ -151,7 +151,7 @@ const PlantRegistrationForm = () => {
           <Paper elevation={2} sx={{ borderRadius: '16px' }} className={styles.paperContainer}>
             <form className={styles.mostOuterConatiner} onSubmit={handleSubmit(onSubmit)}>
               <Box className={styles.formOuterContainer}>
-                <Typography variant="h6" className={styles.heading}>
+                <Typography variant="h4" className={styles.heading}>
                   Plant Registration
                 </Typography>
 
