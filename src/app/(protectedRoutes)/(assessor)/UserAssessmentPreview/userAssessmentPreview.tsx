7f66e67b0a1a8dc04b70abd1bbd682656109828c
harshalelansol <harshald@elansoltech.com>
2025-06-29 21:13:10 +0530

    pull

--- src/app/(protectedRoutes)/(assessor)/UserAssessmentPreview/userAssessmentPreview.tsx
+++ src/app/(protectedRoutes)/(assessor)/UserAssessmentPreview/userAssessmentPreview.tsx
@@ -22,21 +22,7 @@ import { useChangeQuestionsStatusMutation, useStartAssessmentRuleEngineMutation 
 import { setPlantAssessmentDepartment } from '../../(plantAssessment)/plantAssementSlice';
 import { QuestionVerificationStatus } from '@/constants/enums';
 import { triggerToast } from '@/app/utils/toast';
-// const departmentName = [
-//   'R&D',
-//   'Planning',
-//   'Production',
-//   'Quality',
-//   'Maintenance',
-//   'Supply Chain - Sales',
-//   'Supply Chain - Purchase',
-//   'Finance',
-//   'Utilities',
-//   'IT',
-//   'Learning & Development',
-//   'Management',
-//   'HR',
-// ];
+
 const UserAssessmentPreview = () => {
   const params = useParams();
   const router = useRouter();
@@ -63,21 +49,38 @@ const UserAssessmentPreview = () => {
     dispatch(setPlantAssessmentDepartment(''));
   }, [dispatch]);
 
+  const departmentName = [
+    'R&D',
+    'Planning',
+    'Production',
+    'Quality',
+    'Maintenance',
+    'Supply Chain - Sales',
+    'Supply Chain - Purchase',
+    'Finance',
+    'Utilities',
+    'IT',
+    'Learning & Development',
+    'Management',
+    'HR',
+  ];
+
   useEffect(() => {
     const fetchAllDepartmentQuestions = async () => {
       try {
         const all: Question[] = [];
 
-        const result = await getQuestionnairesList({
-          tenantId: organisationId,
-          plantId: plantId || '',
-          // department: dept,
-        }).unwrap();
-        all.push(...(result?.questionsToSend || []));
+        for (const dept of departmentName) {
+          const result = await getQuestionnairesList({
+            tenantId: organisationId,
+            plantId: plantId || '',
+            department: dept,
+          }).unwrap();
+          all.push(...(result?.questionsToSend || []));
+        }
 
         const grouped: { [key: string]: Question[] } = {};
         const justification: { [key: string]: string } = {};
-        console.log('grouped questions', grouped);
 
         all.forEach((q) => {
           // Create a unique composite key
@@ -111,10 +114,7 @@ const UserAssessmentPreview = () => {
     };
 
     fetchAllDepartmentQuestions();
-  }, [getQuestionnairesList, organisationId, plantId]);
-
-  console.log('set grouped questions', groupedQuestions);
-  console.log('set setAllQuestions questions', allQuestions);
+  }, []);
 
   const handleAnswerClick = (answerId: string) => {
     if (!isEditMode) return;
