7a3b2cd90d42312736f7a75dd367f728bdf631cc
shubhamtelansol <shubhamt@elansoltech.com>
2025-07-17 14:37:48 +0530

    harshal changes in plantdetaillist

--- src/components/previewSideBox/AssessorQuestionSection.tsx
+++ src/components/previewSideBox/AssessorQuestionSection.tsx
@@ -19,7 +19,6 @@ const AssessorQuestionSection: React.FC<Props> = ({ questions, selectedQuestionI
       {questions.map((q) => {
         if (!q.key) return null;
         const isSelected = q.key === selectedQuestionId;
-        const isCompleted = completedIds.includes(q.key);
 
         // Add verification status check
         const isVerified = q.verificationStatus === QuestionVerificationStatus.ASSESSOR_VERIFIED;
@@ -28,7 +27,7 @@ const AssessorQuestionSection: React.FC<Props> = ({ questions, selectedQuestionI
 
         const classNames = [
           styles.questionButton,
-          // isCompleted ? styles.reviewed : '',
+
           isSelected ? styles.selected : '',
           q.verificationStatus && isVerified ? styles.isVerified : '',
           q.verificationStatus && isNotVerified ? styles.notVerified : '',
